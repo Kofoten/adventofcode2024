85fa55e8c7be2ec8de28e735d00cab30aad5cef5
Rasmus Steen <rasmus@steen.ninja>
2024-12-13 22:35:15 +0100

    fixing compilation issues

--- aoc24/Challenges/Challenge09/Challenge09.cs
+++ aoc24/Challenges/Challenge09/Challenge09.cs
@@ -67,69 +67,6 @@ public class Challenge09(IOptions options) : IChallenge
     public async Task<string> Part2(IInputReader reader, CancellationToken cancellation)
     {
         var discMap = await reader.ReadToEndAsync(cancellation);
-        var disc = new List<(int, int, long)>();
-
-        var index = 0;
-        var fileId = 0L;
-        var isFile = true;
-        foreach (var b in discMap)
-        {
-            var value = (int)char.GetNumericValue(b);
-            if (isFile)
-            {
-                disc.Add((index, value, fileId));
-                fileId++;
-            }
-
-            isFile = !isFile;
-            index += value;
-        }
-
-        if (options.Verbose)
-        {
-            Print(disc, index);
-        }
-
-        for (int i = disc.Count - 1; i > 0; i--)
-        {
-            var start = 0;
-            var current = disc[i];
-
-            while (start < disc[i].Item1)
-            {
-                var move = (start, current.Item2, current.Item3);
-                var intersecting = disc.FindIndex(x => Intersects(x, move));
-                if (intersecting == -1)
-                {
-                    disc[i] = move;
-                    break;
-                }
-
-                start = disc[intersecting].Item1 + disc[intersecting].Item2;
-            }
-
-            if (options.Verbose)
-            {
-                Print(disc, index);
-            }
-        }
-
-        var answer = 0L;
-        foreach (var file in disc)
-        {
-            for (int i = 0; i < file.Item2; i++)
-            {
-                var position = file.Item1 + i;
-                answer += file.Item3 * position;
-            }
-        }
-
-        return answer.ToString();
-    }
-
-    public async Task<string> Part2Retry(IInputReader reader, CancellationToken cancellation)
-    {
-        var discMap = await reader.ReadToEndAsync(cancellation);
         var disc = new List<int?>();
 
         var fileId = 0;
@@ -159,101 +96,80 @@ public class Challenge09(IOptions options) : IChallenge
         }
 
         int? fileEnd = null;
-        for (int i = disc.Count; i > 0; --i)
+        for (int i = disc.Count - 1; i > 0; i--)
         {
             if (disc[i] is null)
             {
                 continue;
             }
 
-            if (fileEnd is null)
+            fileEnd ??= i;
+            var current = disc[i]!.Value;
+            if (disc[i - 1] != current)
             {
-                fileEnd = i;
+                var fileSize = 1 + fileEnd - i;
+                int? emptyBlockStart = null;
+                for (int j = 0; j < i; j++)
+                {
+                    if (emptyBlockStart is null)
+                    {
+                        if (disc[j] is null)
+                        {
+                            emptyBlockStart = j;
+                        }
+                    }
+                    else if (j - emptyBlockStart == fileSize)
+                    {
+                        for (int k = 0; k < fileSize; k++)
+                        {
+                            disc[k + emptyBlockStart.Value] = current;
+                            disc[k + i] = null;
+                        }
+                        break;
+                    }
+                    else if (disc[j] is not null)
+                    {
+                        emptyBlockStart = null;
+                    }
+                }
+
+                fileEnd = null;
             }
-
-
-            while (disc[last] is null)
-            {
-                last--;
-            }
-
-            disc[i] = disc[last];
-            disc[last] = null;
 
             if (options.Verbose)
             {
                 Print(disc);
             }
         }
-    }
-
-    var answer = 0L;
-        for (int i = 0; disc[i] is not null; i++)
+       
+        var answer = 0L;
+        for (int i = 0; i < disc.Count; i++)
         {
-            answer += (long) disc[i]!.Value* i;
-}
+            if (disc[i].HasValue)
+            {
+                answer += (long)disc[i]!.Value * i;
+            }
+        }
 
         return answer.ToString();
     }
 
-    private static bool Intersects((int, int, long) first, (int, int, long) second)
-{
-    var (firstStart, firstLength, _) = first;
-    var (secondStart, secondLength, _) = second;
+    private static void Print(IList<int?> disc)
+    {
+        var sb = new StringBuilder();
 
-    if (firstStart == secondStart)
-    {
-        return true;
-    }
-    else if (firstStart < secondStart)
-    {
-        return firstStart + firstLength > secondStart;
-    }
-    else
-    {
-        return firstStart < secondStart + secondLength;
-    }
-}
-
-private static void Print(IList<int?> disc)
-{
-    var sb = new StringBuilder();
-
-    for (int i = 0; i < disc.Count; i++)
-    {
-        if (disc[i] is null)
+        for (int i = 0; i < disc.Count; i++)
         {
-            sb.Append('.');
+            if (disc[i] is null)
+            {
+                sb.Append('.');
+            }
+            else
+            {
+                sb.Append(disc[i]);
+            }
         }
-        else
-        {
-            sb.Append(disc[i]);
-        }
+
+        Console.WriteLine(sb.ToString());
     }
-
-    Console.WriteLine(sb.ToString());
-}
-
-private static void Print(IList<(int, int, long)> disc, int length)
-{
-    var sb = new StringBuilder();
-
-    for (int i = 0; i < length;)
-    {
-        var found = disc.Where(x => x.Item1 == i);
-        if (found.Any())
-        {
-            var file = found.First();
-            sb.Append(Enumerable.Repeat(file.Item3.ToString(), file.Item2).Aggregate(string.Empty, (acc, x) => acc + x));
-            i += file.Item2;
-        }
-        else
-        {
-            sb.Append('.');
-            i++;
-        }
-    }
-
-    Console.WriteLine(sb.ToString());
-}
 }
